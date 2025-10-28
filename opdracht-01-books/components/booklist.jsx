cdb3b5601062558af35de6a420831d2db763431c
9024514 <9024514@student.zadkine.nl>
2025-10-28 15:29:01 +0100

    opdracht 1.2 books

--- opdracht-01-books/components/booklist.jsx
+++ opdracht-01-books/components/booklist.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
-import Book from './Book.jsx';
-import books from './data/Books.js';
-import styles from '../styles/BookList.module.css';
+import { useState } from 'react';
+import Book from './Book';
+import '../styles/BookList.css'; 
 
-/**
- * Container component die alle boeken toont (top 3).
- * Kan later eenvoudig worden uitgebreid.
- */
 const BookList = () => {
+  const [books, setBooks] = useState([
+    {
+      id: 1,
+      title: 'Een voor een',
+      author: 'Freida McFadden',
+      image: '/images/een voor een (freida).jpg',
+    },
+    {
+      id: 2,
+      title: 'Het ultieme geheim',
+      author: 'Dan Brown',
+      image: '/images/het ultieme geheim (dan brown).jpg',
+    },
+    {
+      id: 3,
+      title: 'De zwevende wereld',
+      author: 'Annejet van der Zijl',
+      image: '/images/de zwevende wereld (annejet).jpg',
+    },
+  ]);
+
   return (
-    <div className={styles.bookList}>
+    <div className="book-list">
       {books.map((book) => (
         <Book
-          key={book.id}
+          key={book.id} // belangrijk voor React lists
           title={book.title}
           author={book.author}
           image={book.image}
